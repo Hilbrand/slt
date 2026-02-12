189524d28691342dffad4ef43fb8714bccbbc0d9
Hilbrand Bouwkamp <hilbrand@h72.nl>
2026-02-12 21:19:28 +0100

    Hernoemen van total naar totaal was niet goed doorgevoerd. Tevens andere variabelen nl naam gegeven.

--- src/ts/gemeenteStyle.ts
+++ src/ts/gemeenteStyle.ts
@@ -5,10 +5,10 @@ import Fill from "ol/style/Fill";
 import Stroke from "ol/style/Stroke";
 import Style from "ol/style/Style";
 
-export const color_onbekend = "#f0e442";
-const color_ja = "#009e73";
-const color_nee = "#cc79a7";
-const color_geen_gegevens = "#7774";
+export const kleur_onbekend = "#f0e442";
+const kleur_ja = "#009e73";
+const kleur_nee = "#cc79a7";
+const kleur_geen_gegevens = "#7774";
 const kleur_geen_verkiezing = "#fff";
 
 export function isBovenPercentage(data: GemeenteDataType, gem: string, tg: ToegankelijkhedenID | undefined, percentage: number) {
@@ -29,22 +29,22 @@ export function createGemeenteStyleFunction(
 ) {
   return (feature: FeatureLike, _resolution: number): Style => {
     const gem = feature.get("c");
-    const { total, bovenPercentage } = isBovenPercentage(data.value, gem, tg.value, percentage.value);
+    const { totaal, bovenPercentage } = isBovenPercentage(data.value, gem, tg.value, percentage.value);
     let kleur: string;
 
     if (bovenPercentage) {
-      kleur = color_ja;
+      kleur = kleur_ja;
     } else if (data.value[gem]) {
       // @ts-expect-error Bij gemeenten waar geen verkiezingen wordt gehouden staat in kolom 1 de waarde 'geen verkiezing' ipv een getal
       if (data.value[gem][1] === GEEN_VERKIEZING) {
         kleur = kleur_geen_verkiezing;
       } else {
         kleur = data.value[gem][2]
-            ? (data.value[gem][2][tg.value]?.n === total ? color_nee : color_onbekend)
-            : color_geen_gegevens;
+            ? (data.value[gem][2][tg.value]?.n === totaal ? kleur_nee : kleur_onbekend)
+            : kleur_geen_gegevens;
       }
     } else {
-      kleur = color_geen_gegevens;
+      kleur = kleur_geen_gegevens;
     }
     return new Style({
       fill: new Fill({
